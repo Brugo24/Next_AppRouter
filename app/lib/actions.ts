7d3a6c23a696e2a1d195ddc5687b2af308a19269
Brugo24 <bruno.monchelato@unesp.br>
2026-04-22 15:50:49 -0300

    tratamento de erros no invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,17 @@ export async function createInvoice (formData : FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
+    try{
+        await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+    }catch(error){
+        console.error(error);
+        return{
+            message: 'Database Error: Failed to Create Invoice'
+        }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboardI/invoices');
@@ -46,17 +53,26 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
+    try{
+        await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id} 
     `;
+    }catch(error){
+        console.error(error);
+        return{
+            message: 'Database error: Failed to Update the Invoice'
+        }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+
     await sql`
         DELETE from invoices
         WHERE id = ${id}
