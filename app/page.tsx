2d625d53d239f139ee6fdf524595a16c18d0e470
Brugo24 <bruno.monchelato@unesp.br>
2026-04-30 16:03:01 -0300

    mudança na página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -20,11 +20,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className={stylesa.shape}  />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Acme.</strong> Your financial dashboard for invoices and customer management.
           </p>
           <Link
             href="/login"
